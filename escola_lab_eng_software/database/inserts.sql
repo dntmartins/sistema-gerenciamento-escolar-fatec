aa756ca4894af902df48c60609987dcb6d31bd80
Hugo <hrichardvieira@outlook.com>
2015-10-26 21:51:18 -0200

    Atualização do modelo

---
+++ escola_lab_eng_software/database/inserts.sql
@@ -0,0 +1,9 @@
+#INSERT USER
+
+INSERT INTO `SchoolProject`.`user` (`userid`, `login`, `password`, `name`, `cpf`) VALUES (1, 'hugo', '88016695', 'Hugo Richard', '11122277796');
+INSERT INTO `SchoolProject`.`user` (`userid`, `login`, `password`, `name`, `cpf`) VALUES (2, 'dante', '88016691', 'Dante', '88822277796');
+INSERT INTO `SchoolProject`.`user` (`userid`, `login`, `password`, `name`, `cpf`) VALUES (3, 'ewerton', '88016692', 'Ewerton', '55522277796');
+INSERT INTO `SchoolProject`.`user` (`userid`, `login`, `password`, `name`, `cpf`) VALUES (4, 'joao', '88016690', 'João Pedro', '66622277796');
+INSERT INTO `SchoolProject`.`user` (`userid`, `login`, `password`, `name`, `cpf`) VALUES (5, 'pedro', '88016698', 'Pedro Alves', '44422277796');
+
+SELECT * FROM user;
