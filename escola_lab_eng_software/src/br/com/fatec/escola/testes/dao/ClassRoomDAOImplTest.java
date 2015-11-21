196c8f5b507c1c8d6d326b198b2a7460aa78a024
Dante Martins <dntmartins@gmail.com>
2015-11-21 14:03:01 -0200

    Finalização dos testes da ClassRoomDAO

--- escola_lab_eng_software/src/br/com/fatec/escola/testes/dao/ClassRoomDAOImplTest.java
+++ escola_lab_eng_software/src/br/com/fatec/escola/testes/dao/ClassRoomDAOImplTest.java
@@ -31,10 +31,11 @@ public class ClassRoomDAOImplTest extends EscolaBaseTest {
 	}
 	
 	@Test
-	public void testSave() {
+	public void testSave() { //OK
 		Module m = new Module();
 		Course c = new Course();
 		Discipline d = new Discipline();
+		ClassRoom cR = new ClassRoom();
 		DisciplineDAOImpl dDAO = new DisciplineDAOImpl();
 		ModuleDAOImpl mDAO = new ModuleDAOImpl();
 		CourseDAOImpl cDAO = new CourseDAOImpl();
@@ -49,8 +50,142 @@ public class ClassRoomDAOImplTest extends EscolaBaseTest {
 		d.setModule(mSaved);
 		d.setName("Portugues");
 		Discipline dSaved = dDAO.save(d);
+		cR.setName("Sala 402");
+		cR.setDiscipline(dSaved);
+		cR.setModule(mSaved);
+		ClassRoom cRSaved = this.dao.save(cR);
+		this.dao.delete(cRSaved);
+		dDAO.delete(dSaved);
 		mDAO.delete(mSaved);
 		cDAO.delete(cSaved);
+	}
+	
+	@Test
+	public void testFindBy() { //OK
+		Module m = new Module();
+		Course c = new Course();
+		Discipline d = new Discipline();
+		ClassRoom cR = new ClassRoom();
+		DisciplineDAOImpl dDAO = new DisciplineDAOImpl();
+		ModuleDAOImpl mDAO = new ModuleDAOImpl();
+		CourseDAOImpl cDAO = new CourseDAOImpl();
+		c.setBeginHour("07:00");
+		c.setEndHour("12:00");
+		c.setCourseDuration(6);
+		c.setName("Analise de Sistemas");
+		Course cSaved = cDAO.save(c);
+		m.setCourse(cSaved);
+		m.setName("1 semestre");
+		Module mSaved = mDAO.save(m);
+		d.setModule(mSaved);
+		d.setName("Portugues");
+		Discipline dSaved = dDAO.save(d);
+		cR.setName("Sala 402");
+		cR.setDiscipline(dSaved);
+		cR.setModule(mSaved);
+		ClassRoom cRSaved = this.dao.save(cR);
+		cR = this.dao.findById(cRSaved.getId());
+		this.dao.delete(cRSaved);
 		dDAO.delete(dSaved);
+		mDAO.delete(mSaved);
+		cDAO.delete(cSaved);
+		assertEquals(cR.getId(), cRSaved.getId());		
+	}
+	
+	@Test
+	public void testFindAllTest() { //OK
+		Module m = new Module();
+		Course c = new Course();
+		Discipline d = new Discipline();
+		ClassRoom cR = new ClassRoom();
+		DisciplineDAOImpl dDAO = new DisciplineDAOImpl();
+		ModuleDAOImpl mDAO = new ModuleDAOImpl();
+		CourseDAOImpl cDAO = new CourseDAOImpl();
+		c.setBeginHour("07:00");
+		c.setEndHour("12:00");
+		c.setCourseDuration(6);
+		c.setName("Analise de Sistemas");
+		Course cSaved = cDAO.save(c);
+		m.setCourse(cSaved);
+		m.setName("1 semestre");
+		Module mSaved = mDAO.save(m);
+		d.setModule(mSaved);
+		d.setName("Portugues");
+		Discipline dSaved = dDAO.save(d);
+		cR.setName("Sala 402");
+		cR.setDiscipline(dSaved);
+		cR.setModule(mSaved);
+		ClassRoom cRSaved = this.dao.save(cR);
+		List<ClassRoom> cRList = this.dao.findAll();
+		this.dao.delete(cRSaved);
+		dDAO.delete(dSaved);
+		mDAO.delete(mSaved);
+		cDAO.delete(cSaved);
+		assertEquals(cRList.size(), 2);
+	}
+	
+	@Test
+	public void testUpdate() { //OK
+		Module m = new Module();
+		Course c = new Course();
+		Discipline d = new Discipline();
+		ClassRoom cR = new ClassRoom();
+		DisciplineDAOImpl dDAO = new DisciplineDAOImpl();
+		ModuleDAOImpl mDAO = new ModuleDAOImpl();
+		CourseDAOImpl cDAO = new CourseDAOImpl();
+		c.setBeginHour("07:00");
+		c.setEndHour("12:00");
+		c.setCourseDuration(6);
+		c.setName("Analise de Sistemas");
+		Course cSaved = cDAO.save(c);
+		m.setCourse(cSaved);
+		m.setName("1 semestre");
+		Module mSaved = mDAO.save(m);
+		d.setModule(mSaved);
+		d.setName("Portugues");
+		Discipline dSaved = dDAO.save(d);
+		cR.setName("Sala 402");
+		cR.setDiscipline(dSaved);
+		cR.setModule(mSaved);
+		ClassRoom cRSaved = this.dao.save(cR);
+		cRSaved.setName("Sala 408");
+		cR = this.dao.update(cRSaved);
+		this.dao.delete(cRSaved);
+		dDAO.delete(dSaved);
+		mDAO.delete(mSaved);
+		cDAO.delete(cSaved);
+		assertEquals("Sala 408", cR.getName());
+	}
+	
+	@Test
+	public void testDelete() { //OK
+		Module m = new Module();
+		Course c = new Course();
+		Discipline d = new Discipline();
+		ClassRoom cR = new ClassRoom();
+		DisciplineDAOImpl dDAO = new DisciplineDAOImpl();
+		ModuleDAOImpl mDAO = new ModuleDAOImpl();
+		CourseDAOImpl cDAO = new CourseDAOImpl();
+		c.setBeginHour("07:00");
+		c.setEndHour("12:00");
+		c.setCourseDuration(6);
+		c.setName("Analise de Sistemas");
+		Course cSaved = cDAO.save(c);
+		m.setCourse(cSaved);
+		m.setName("1 semestre");
+		Module mSaved = mDAO.save(m);
+		d.setModule(mSaved);
+		d.setName("Portugues");
+		Discipline dSaved = dDAO.save(d);
+		cR.setName("Sala 402");
+		cR.setDiscipline(dSaved);
+		cR.setModule(mSaved);
+		ClassRoom cRSaved = this.dao.save(cR);
+		this.dao.delete(cRSaved);
+		cR = this.dao.findById(cRSaved.getId());
+		dDAO.delete(dSaved);
+		mDAO.delete(mSaved);
+		cDAO.delete(cSaved);
+		Assert.assertNull(cR);	
 	}
 }
